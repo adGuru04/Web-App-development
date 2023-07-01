b0d462976136d39afd87144abc6cca4256313701
Guru <adGuru04>
2023-07-01 18:28:40 +0530

    dynamic title, path added, changes in main layout in routes

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use(shoproutes);
 
 
 app.use((req, res, next) => {
-        res.status(404).render('404');
+        res.status(404).render('404', {pageTitle:'Page not found'});
 });
 
            
