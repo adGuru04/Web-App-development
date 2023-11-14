8bc3ce8a89f17edf7b6490a3eac23cfad175480e
Guru <adGuru04>
2023-11-15 03:09:39 +0530

    adding model, view, controllers, modifying and restructuring the views, removing the residual FE files

--- app.js
+++ app.js
@@ -2,14 +2,13 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
-//const expressHbs = require('express-handlebars');
 
 const errorController = require('./controllers/error');
 
 const app = express();
 
 app.set('view engine', 'ejs');
-app.set('views', './views');
+app.set('views', 'views');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
@@ -22,4 +21,4 @@ app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-app.listen(2000);       
+app.listen(2000);
--- routes/shop.js
+++ routes/shop.js
@@ -2,15 +2,10 @@ const path = require('path');
 
 const express = require('express');
 
-
 const productsController = require('../controllers/products');
+
 const router = express.Router();
-//const router = express.Router();
-//const adminData = require('./admin');
 
-
-// products shown/got on the shop
-router.get('/', productsController.getProduct);
-
+router.get('/', productsController.getProducts);
 
 module.exports = router;
